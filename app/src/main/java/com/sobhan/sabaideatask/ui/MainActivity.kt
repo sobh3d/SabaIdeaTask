02dab3132d0b8e54ebd6f2236a83c4c6e6574f05
sobhan hasnavnd <sob.hasanvand@gmail.com>
2022-07-04 17:30:33 +0430

    add view model to activity and activity binding

--- app/src/main/java/com/sobhan/sabaideatask/ui/MainActivity.kt
+++ app/src/main/java/com/sobhan/sabaideatask/ui/MainActivity.kt
@@ -2,11 +2,19 @@ package com.sobhan.sabaideatask.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.sobhan.sabaideatask.R
+import com.sobhan.sabaideatask.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val movieViewModel: MovieViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
